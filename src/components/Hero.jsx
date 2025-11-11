a2119ada702bf475804d5f686600c635f60f2619
nico-hondo <nicohondo01@gmail.com>
2025-11-12 04:25:00 +0700

    style(ui): add styling for hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,13 @@
 "use client";
 
+import Image from "next/image";
+
+import HeroImage from "../../public/images/Background-Image.png";
+
+import { IoLocationOutline } from "react-icons/io5";
+import { CiCalendar } from "react-icons/ci";
+import { IoIosSend } from "react-icons/io";
+
 export default function Hero(){
 
     // Configure Whatsapp Link
@@ -10,13 +18,42 @@ export default function Hero(){
 
 
     return(
-        <section className="hero-section h-screen w-full flex items-center justify-center">
-            <div className="bg-hero min-h-screen h-full w-full flex items-center justify-center text-center">
-                <div className="grid grid-rows-2 gap-8">
-                    <h1 className="font-libre font-bold text-4xl md:text-5xl row-span-1">Discover Serenity at <br/> Tolping's Villa</h1>
-                    <a href={whatsappLinkCSfirst} className="font-roboto row-span-1 h-12 w-35 mx-auto bg-(--color-button) text-(--color-base) font-semibold rounded-3xl flex items-center justify-center hover:bg-(--color-badge) hover:text-(--color-nav) transition duration-300 text-sm">
-                        Book Now
-                    </a>
+        <section className="w-full h-screen flex items-center justify-center relative">
+
+            <Image src={HeroImage} alt="Hero-Image" className="w-full h-full inset-0 object-cover absolute"/>
+
+            {/* Overlay background - linear gradient -> to right */}
+            <div className="absolute inset-0 bg-linear-to-r from-slate-800/80 via-slate-800/50 to-transparent"></div>
+
+            <div className="w-full h-auto px-6 md:px-12 lg:px-20 z-10">
+                <div className="max-w-6xl mx-auto px-4 flex flex-col gap-48 relative">
+                    <div className="w-full bg-white py-4 px-8 grid grid-cols-3 gap-4 rounded-2xl shadow-lg">
+                        <div className="flex flex-col border-r border-dashed border-indigo-500 justify-center px-4">
+                            <div className="flex flex-row gap-2 items-center">
+                                <IoLocationOutline className="text-(--color-button) text-sm"/>
+                                <span className="text-sm text-gray-600 font-nunito-sans tracking-widest">Plan Your Stay</span>
+                            </div>
+                            <h3 className="text-2xl font-montaga font-medium text-gray-500">Pick your stay dates!</h3>
+                        </div>
+                        <div className="flex flex-col justify-center">
+                            <div className="flex flex-row gap-2 items-center">
+                                <CiCalendar className="text-(--color-button) text-sm"/>
+                                <span className="text-sm text-gray-600 font-nunito-sans tracking-widest">1 Night</span>
+                            </div>
+                            <h3 className="text-2xl font-montaga font-medium text-black cursor-pointer">Wed, 12 Nov - Thu, 13 Nov</h3>
+                        </div>
+                        <div className="w-full flex fle-row gap-2 items-center bg-black/90 rounded-full justify-center text-white cursor-pointer hover:bg-(--color-button) hover:text-black transition duration-300 py-4">
+                            <IoIosSend className="text-lg"/>
+                            <span>Check Availability</span>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-8">
+                        <h1 className="font-villatolping font-medium text-4xl md:text-5xl ">Discover Serenity at <br/> Tolping's Villa</h1>
+                        <a href={whatsappLinkCSfirst} className="font-roboto h-12 w-35 bg-(--color-button) text-(--color-base) font-semibold rounded-3xl flex items-center justify-center hover:bg-(--color-badge) hover:text-(--color-nav) transition duration-300 text-sm">
+                            Book Now
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
